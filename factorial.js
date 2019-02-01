637a7c8a708e93fb9c1f12a79dcb7a49b89dfb72
prakhars98 <43076632+prakhars98@users.noreply.github.com>
2019-02-01 06:39:32 -0800

    Add files via upload

---
+++ factorial.js
@@ -0,0 +1,29 @@
+
+var factorial = 1; var inputValue;
+function factorialNo() { // Calculates the factorial of a number when an event specified in the 
+    // addEventListener method occurs.
+    inputValue = document.getElementById('input-no').value;
+    for (var i = 1; i <= inputValue; i++) {
+        factorial = factorial * i;
+    }
+    document.getElementById('display-panel').innerText = factorial;
+}
+document.querySelector('#calcFactorial').addEventListener('click', factorialNo);//Listens to the click event on the 
+// specified element and calls the func.'factorialNo'
+function isNumber(event) {
+    var charcode = event.charCode;
+    if (charcode < 46 || charcode > 57) {
+        document.querySelector('#display-panel').style.color = 'red';
+        document.querySelector('#display-panel').innerHTML = 'Invalid Number !';
+    }
+    else
+        document.querySelector('#display-panel').innerHTML = '';
+}
+document.querySelector('#input-no').addEventListener('keypress', isNumber);
+function reset() {
+    factorial = 0;
+    document.getElementById('display-panel').innerText = '';
+    document.getElementById('input-no').value = '';
+
+}
+document.querySelector('#Reset-button').addEventListener('click', reset);
